58b898f3f6fe0be569421bd1e4570c2ce17bef85
jaydunb12 <74875595+jaydunb12@users.noreply.github.com>
2022-09-26 16:22:01 -0400

    update

--- tracker/feed.js
+++ tracker/feed.js
@@ -1,43 +1,63 @@
 
-
+// Full Stack Mern project
+//Node express backend
+//Mongodb database
+//npm init
+// npm install express mongoose nodemon
 
 const express = require("express");
 const app = express();
-const path = require("path");
+const path = require("path")
+//Bring in the model
 const Habit = require("./models/habit")
 
+// The middleware of choice is ejs
+// npm install ejs
 
 
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
 
-const mongoose = require('mongoose');
+
+// create a .env file add add ATLAS_URI as the variable with the
+
+//connection string from mongodb
+
+//npm install dotenv
+
+const mongoose = require("mongoose");
 const port = process.env.PORT || 5000;
-
-
-require('dotenv').config();
-
+require("dotenv").config();
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true }
-);
+mongoose.connect(uri, { useNewUrlParser: true})
+
 const connection = mongoose.connection;
-connection.once('open', () => {
-  console.log("MongoDB database connection established successfully");
+connection.once("open", () => {
+    console.log("MongoDB connection establised")
 })
 
+
+
+//Time to make the model that will be loaded
 const item = new Habit({
-    title: "water",
-    number: "1",
-    category: "Nutrition"
+    title: "running",
+    number: "20",
+    category: "Fitness"
 })
 item.save().then(item => {
-    console.log(item)
 })
 .catch(e => {
     console.log(e)
 })
 
+//lets setup the first get route to make sure we can speak
+
+// from the backend to the front
+
+app.get("/habit", async(req, res) => {
+    const habits = await Habit.find({})
+    res.render("home.ejs", {habits})
+})
+
 
 app.listen(5500, () => {
-    console.log("We are going on the port")
+    console.log("We are up and going!")
 })
--- tracker/models/habit.js
+++ tracker/models/habit.js
@@ -1,23 +1,21 @@
+// Create the schema for the model that will be in the mern app
 
-const mongoose = require("mongoose");
-
-
+const mongoose = require("mongoose")
 const habitSchema = new mongoose.Schema({
     title: {
         type: String,
-        required: true
+        required: true,
     },
     number: {
         type: Number,
         required: true,
-        min: 0
+        min: 0,
     },
     category: {
         type: String,
         enum: ["Fitness", "Learning", "Nutrition"]
     }
+
 })
-
-
 const Habit = mongoose.model("Habit", habitSchema);
 module.exports = Habit
